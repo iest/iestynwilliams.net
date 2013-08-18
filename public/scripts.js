c912c2d0e446745f8c8aff3c0f6392d6976dd56d
Iestyn Williams <Iestyn.Williams@eysys.com>
2013-08-18 20:47:38 +0100

    Cleaned up script

--- public/scripts.js
+++ public/scripts.js
@@ -2,8 +2,6 @@ var currentTime = new Date();
 
 currentTime = currentTime.getHours();
 
-console.log(currentTime);
-
-if (currentTime < 6 && currentTime > 20) {
+if (currentTime <= 6 && currentTime >= 20) {
   document.querySelector('html').classList.add('night');
 }
