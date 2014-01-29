967f169527c0183cb276dbf4d3d34e7d1bab833d
Iestyn Williams <iest@eysys.com>
2014-01-29 22:19:19 +0000

    Fixed scripts, nightmode styles that were missing

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,5 @@
 var now = new Date().getHours();
 
 if (now >= 20 || now < 8) {
-  // document.querySelector('html').classList.add('night-mode');
+  document.querySelector('html').classList.add('night-mode');
 }
